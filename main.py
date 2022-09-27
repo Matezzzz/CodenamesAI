c8709c997c05361cdc413ec4407939d80ca06def
Mrázek Matěj <741-mrazekma@users.noreply.localhost>
2022-09-27 15:51:00 +0200

    Add a few comments

--- main.py
+++ main.py
@@ -2,9 +2,12 @@ from players import Game
 from datasets import CombinedInitializer
 
 
-#create a game with given teams
+#let user define both teams
 game = Game.createGameInput(CombinedInitializer.DefaultInitializer())
+
+#create a game with given teams
 #game = Game.createGame(Game.TEAM_AI_H, Game.TEAM_AUTO, CombinedInitializer.DefaultInitializer())
+
 #play 1 game
 game.playRound()
 
